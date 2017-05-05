6521cd312a2e95a596ef2d95935078161f33d7a3
Stephen Anderson <sa3196a@student.american.edu>
2017-05-05 00:37:40 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-(function() {
-  'use strict';
 
-  // YOUR CODE HERE
-})();
+
+var buttons = document.getElementsByClassName('button');
+var bulb = document.getElementsByClassName('bulb');
+
+var lights = ['stop', 'slow', 'go'];
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    bulb[i].classList.toggle(`${lights[i]}`);
+  });
+};
